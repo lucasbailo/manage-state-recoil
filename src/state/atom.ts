7d31e80196e8dca7441fe7059b7b5c4afcd361d6
Lucas Bailo <lucasebailo@gmail.com>
2024-10-29 11:56:51 -0300

    first commit

---
+++ src/state/atom.ts
@@ -0,0 +1,20 @@
+import { atom } from "recoil";
+import { IEvento } from "../interfaces/IEvento";
+
+export const listaDeEventosState = atom<IEvento[]>({
+    key: 'listaDeEventosState',
+    default: [{
+        "descricao": "Estudar React",
+        "inicio": new Date("2024-10-29T09:00"),
+        "fim": new Date("2024-10-29T13:00"),
+        "completo": false,
+        "id": 1642342747
+      },
+      {
+        "descricao": "Estudar Recoil",
+        "inicio": new Date("2024-10-29T09:00"),
+        "fim": new Date("2024-10-29T11:00"),
+        "completo": false,
+        "id": 1642342959
+      }]
+})
